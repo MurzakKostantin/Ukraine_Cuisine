889077bafb6c535ae376c07d81d6349cd31fd256
Костянтин Муржак <murzakkostantin118@gmail.com>
2025-04-23 00:24:46 +0300

    мейбі буде хост з бд і картінки

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const router = express.Router();
 const cors = require("cors");
 const { createClient } = require("@supabase/supabase-js");
 const bcrypt = require("bcryptjs");
